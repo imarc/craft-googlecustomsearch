772f8d9e9ea8311ecb76ec15176b65256d752fcb
Dan Collins <daniel.t.collins@gmail.com>
2015-04-21 17:09:41 -0400

    Setting up plugin file

---
+++ GoogleCustomSearchPlugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Craft;
+
+class GoogleCustomSearchPlugin extends BasePlugin
+{
+	function getName()
+	{
+		return 'Google Custom Search';
+	}
+
+	function getVersion()
+	{
+		return '0.1';
+	}
+
+	function getDeveloper()
+	{
+		return 'iMarc';
+	}
+
+	function getDeveloperUrl()
+	{
+		return 'http://www.imarc.net';
+	}
+}
