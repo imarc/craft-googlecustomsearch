9aa3b60475c42dd9c32f557db191bae6b354d015
Kerri Cranwell <kerri@imarc.com>
2025-02-06 13:31:41 -0500

    Update plugin to use guzzle client and send referrer info; Update settings fields to allow for env variables

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -4,17 +4,28 @@ namespace imarc\googlecustomsearch\models;
 use Craft;
 use craft\base\Model;
 use imarc\googlecustomsearch\Plugin;
+use craft\helpers\App;
 
 class Settings extends Model
 {
     public $apiKey = '';
     public $searchEngineId = '';
 
+    public function getApiKey(): string
+    {
+        return App::parseEnv($this->apiKey);
+    }
+
+    public function getSearchEngineId(): string
+    {
+        return App::parseEnv($this->searchEngineId);
+    }
+
     public function rules(): array
     {
         return [
-            ['searchEngineId', 'string'],
-            ['apiKey', 'string']
+            [['apiKey', 'searchEngineId'], 'required'],
+            [['apiKey', 'searchEngineId'], 'string'],
         ];
     }
 }
